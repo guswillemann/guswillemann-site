9604cb342a856d5524a39c595a2e49191d1e2fc5
Gustavo Willemann <guswillemann@gmail.com>
2021-10-07 04:11:05 -0300

    fix: adds basic validations to the request

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -5,6 +5,12 @@ const EMAIL_ADDRESS = process.env.EMAIL_ADDRESS;
 const EMAIL_PASS = process.env.EMAIL_PASS;
 
 export default function ContactEndPoint(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') return res.status(405).json({ message: 'Use method POST' });
+
+  const isValidReq = req.body.name && req.body.email && req.body.message;
+
+  if (!isValidReq) return res.status(400).json({ message: 'The request has missing data' });
+  
   const transporter = createTransport({
     port: 465,
     host: "smtp.gmail.com",
@@ -18,17 +24,17 @@ export default function ContactEndPoint(req: NextApiRequest, res: NextApiRespons
   const mailData = {
     from: EMAIL_ADDRESS,
     to: 'guswillemann@gmail.com',
-    subject: `Message From ${req.body.name}`,
+    subject: `Message From ${req.body.name} ${Date()}`,
     text: req.body.message,
     html: `<p>${req.body.email}</p><div>${req.body.message}</div>`
   };
   
   transporter.sendMail(mailData, (err) => {
     if(err) {
-      res.status(500).json({ error: 'failed to send the email.' })
+      res.status(500).json({ error: 'Failed to send the email.' })
     }
     else {
-      res.status(200).json({ message: 'email sended with success.' })
+      res.status(200).json({ message: 'Email successfully sent.' })
     }
   });
 }
